6e61c0793933b459bdfd905160f817c8f924d953
Brad Iglesia <31361392+B-Iglesia@users.noreply.github.com>
2018-09-21 13:25:14 -0700

    Update Lab00.java

--- Lab00.java
+++ Lab00.java
@@ -4,19 +4,29 @@ public class Lab00
    public static void main(String[] args)
    {
       int x = 5;String y = "hello";double z = 9.8;
-      System.out.printf("x:",x,"y:",y,"z:",z); 
+      System.out.printf("x:",x);
+      System.out.printf("y:",y);
+      System.out.printf("z:",z); 
 
       
       int[] nums = {3,6,-1,2};
       for (int num : nums)
-         System.out.println(nums[num]);
+         System.out.println(num);
       
       for (int i=1; i<11; i++) {
          System.out.println(i);
       }
       System.out.println("\n");
-      
-      
    }
+      public void char_count(String s, char c) {
+    	  int count = 0;
+    	  for (int i = 0; i < s.length(); i++) {
+    		if (s.charAt(i) == c)
+    			count++;
+    	  }
+      }
+   
       
-   }
+ }
+      
+  
