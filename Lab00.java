eadeea1e7e8d59d45fe41328874eec4d6562057d
Brad Iglesia <31361392+B-Iglesia@users.noreply.github.com>
2018-09-21 13:13:50 -0700

    Update Lab00.java

--- Lab00.java
+++ Lab00.java
@@ -4,18 +4,14 @@ public class Lab00
    public static void main(String[] args)
    {
       int x = 5;String y = "hello";double z = 9.8;
-      System.out.printf("x:",x); 
-      
-      System.out.printf("y:",y); 
-      
-      System.out.printf("z:",y);
-      
+      System.out.printf("x:",x,"y:",y,"z:",z); 
+
       
       int[] nums = {3,6,-1,2};
       for (int num : nums)
          System.out.println(nums[num]);
       
-      for (int i; i<11; i++) {
+      for (int i=1; i<11; i++) {
          System.out.println(i);
       }
       System.out.println("\n");
